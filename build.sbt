5fd2f556400623ee0996f62b2d81543c6c8ae6cf
Daniel <danielbernal@seen4n.com>
2017-02-14 12:07:55 -0500

    akka-http added

--- build.sbt
+++ build.sbt
@@ -23,18 +23,24 @@ libraryDependencies ++= {
 
   val akkaV = "2.4.4"
   val phantomV = "2.0.2"
+  val akkaHttpV   = "10.0.3"
+  val scalaTestV  = "3.0.1"
 
   Seq(
     "com.outworkers"      %%  "phantom-dsl"                 % phantomV,
     "com.outworkers"      %%  "phantom-streams"             % phantomV,
     "com.websudos"        %%  "util-testing"                % "0.13.0"    % "test, provided",
-    "org.scalatest"       %%  "scalatest"                   % "2.2.4"     % "test",
-    "com.typesafe.akka"   %%  "akka-actor"                  % akkaV,
-    "com.typesafe.akka"   %%  "akka-stream"                 % akkaV,
     "com.typesafe.akka"   %%  "akka-slf4j"                  % akkaV,
-    "com.typesafe.akka"   %%  "akka-testkit"                % akkaV,
     "com.typesafe.akka"   %%  "akka-stream-testkit"         % akkaV,
-    "com.typesafe.play"   %%  "play-streams-experimental"   % "2.4.6"
+    "com.typesafe.play"   %%  "play-streams-experimental"   % "2.4.6",
+    "com.typesafe.akka"   %%  "akka-actor"                   % akkaV,
+    "com.typesafe.akka"   %%  "akka-stream"                  % akkaV,
+    "com.typesafe.akka"   %%  "akka-testkit"                 % akkaV,
+    "com.typesafe.akka"   %%  "akka-http"                    % akkaHttpV,
+    "com.typesafe.akka"   %%  "akka-http-spray-json"         % akkaHttpV,
+    "com.typesafe.akka"   %%  "akka-http-testkit"            % akkaHttpV,
+    "org.scalatest"       %%  "scalatest"                    % scalaTestV % "test"
+
   )
 }
 
--- src/main/scala/controller/WebServer.scala
+++ src/main/scala/controller/WebServer.scala
@@ -1,32 +1,51 @@
 package controller
 
-
-import java.util.UUID
-
-import com.outworkers.phantom.dsl.ResultSet
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import persistence.User
 import service.UserService
+import spray.json.DefaultJsonProtocol
 
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
-import scala.concurrent._
-import ExecutionContext.Implicits.global
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
 
+import scala.io.StdIn
 
-object WebServer {
+object WebServer extends SprayJsonSupport with DefaultJsonProtocol {
 
   def main(args: Array[String]) {
-    print ("\n\n\n Hello, let's try to bring the saved user with id: 1")
-    val future :Future[Option[User]] = UserService.getUserById(1)
-    future onComplete {
-      case Success (smth) => print ("\n\n User Found: " +smth.getOrElse())
-      case Failure (err) => print (err)
-    }
 
-    val future2 : Future[ResultSet] = UserService.storeUser(User(UUID.randomUUID().hashCode(), "Pepito", "Perez"))
-    future2 onComplete {
-      case Success (smth) => print ("\n\n User added correctly")
-      case Failure (err) => print (err)
-    }
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    implicit val userFormat = jsonFormat3(User)
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("hello") {
+        get {
+          onSuccess(findUser()) {
+            case Some(user) => complete(StatusCodes.OK, user)
+            case None => complete(StatusCodes.NotFound)
+          }
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
   }
+
+  def findUser() : Future[Option[User]] = {
+        UserService.getUserById(2)
+  }
+
 }
