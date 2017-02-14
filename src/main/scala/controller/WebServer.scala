810129b376ab9ed2dac2875f796a6bd566a8bbb3
Daniel <danielbernal@seen4n.com>
2017-02-14 12:15:25 -0500

    anonymous pattern matching in return

--- src/main/scala/controller/WebServer.scala
+++ src/main/scala/controller/WebServer.scala
@@ -37,11 +37,6 @@ object WebServer extends SprayJsonSupport with DefaultJsonProtocol {
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 
   def findUser() : Future[Option[User]] = {
